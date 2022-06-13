ae6281b39973051b3459ae711cc4799c2650d6a1
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-13 11:05:03 +0300

    added navbar.html

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def navbar(request):
+    return render(request, 'navbar.html')
+
+
+def login_user(request):
+    return render(request, 'login.html')
