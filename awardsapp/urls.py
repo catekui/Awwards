b43d0404128c61c609ebefb84769bb44ffbabc1f
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-13 12:49:17 +0300

    added and styled user authenticate

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.base, name='base'),
     path('navbar/', views.navbar, name='navbar'),
+    # path('login/', views.login, name='login'),
+    path('login/', views.loginPage, name="login"),  
+    path('register/', views.register, name='register'),
    
 ]
