20331c7b2656f822c720f22bd6db8765d01e1204
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-13 16:51:55 +0300

    added class Project

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    date_created = models.DateTimeField(auto_now_add=True)
+
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')  
     bio = models.TextField(max_length=400, blank=True,null=True)
+    email = models.CharField(max_length=400, null=True, blank=True)
+    project = models.CharField
+
+    
     
     def save_profile(self):
         self.save()
@@ -25,5 +26,18 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+# class User(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField(max_length=100)
+#     date_created = models.DateTimeField(auto_now_add=True)
+
+class projects(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=400)
+    image = models.ImageField(max_length=500)
+    description = models.TextField
+    link = models.URLField(max_length=1000)
 
 
