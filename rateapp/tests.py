37a49a22b31637b2a53bf5f869a0084a336a5a33
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-14 15:17:08 +0300

    refactor the codebase

--- rateapp/tests.py
+++ rateapp/tests.py
@@ -33,3 +33,17 @@ class ProfileTestClass(TestCase):
     profile = Profile(bio="sometext")
     self.assertEqual(str(profile), 'sometext')
 
+class ReviewTestClass(TestCase):
+
+  # Set up method
+  def setUp(self):
+    self.rev= Review(bio = 'Titl1', prof_pic = 'profpic.png' )
+
+  # Testing  instance
+  def test_instance(self):
+    self.assertTrue(isinstance(self.prof,Profile))
+
+  # Testing String representation Method
+  def test_string_representation(self):
+    profile = Profile(bio="sometext")
+    self.assertEqual(str(profile), 'sometext')
