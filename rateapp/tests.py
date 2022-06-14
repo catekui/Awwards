622b95e6ca4aa568f17f5f38cbef25e5360f69e7
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-14 15:14:29 +0300

    updated changes

--- rateapp/tests.py
+++ rateapp/tests.py
@@ -18,20 +18,7 @@ class ProjectTestClass(TestCase):
     self.assertEqual(str(project), 'sometext')
 
 
-class ProfileTestClass(TestCase):
 
-  # Set up method
-  def setUp(self):
-    self.prof= Profile(bio = 'Titl1', prof_pic = 'profpic.png' )
-
-  # Testing  instance
-  def test_instance(self):
-    self.assertTrue(isinstance(self.prof,Profile))
-
-  # Testing String representation Method
-  def test_string_representation(self):
-    profile = Profile(bio="sometext")
-    self.assertEqual(str(profile), 'sometext')
 
 class ReviewTestClass(TestCase):
 
