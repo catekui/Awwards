58fe7d96ffa15425d247ae2b714f359c20a7f4bf
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-14 15:22:16 +0300

    debugging

--- rate/settings.py
+++ rate/settings.py
@@ -112,9 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Africa/Nairobi'
@@ -126,8 +123,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
--- rate/urls.py
+++ rate/urls.py
@@ -20,8 +20,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('rateapp.urls')),
-    # url(r'^tinymce/', include('tinymce.urls')),
-    # url(r'^accounts/', include('registration.backends.simple.urls')),
+
   
     
 ]
